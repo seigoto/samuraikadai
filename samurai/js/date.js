2dde5bfce6a60cb859475afb516ed9c5e1dabbe4
seigoto <164344442+seigoto@users.noreply.github.com>
2024-03-21 21:33:05 +0900

    Add files via upload

---
+++ samurai/js/date.js
@@ -0,0 +1,7 @@
+    const Btn = document.getElementById('btn');
+    
+    Btn.addEventListener('click',() =>{
+        setTimeout(()=>{
+        text.textContent = 'くりっくしました。';
+    },2000);
+    });
